7d500f6332bd3dc000cd705c15240bfac96f8314
Rodrigue Geis <rodrigue.geis@open-xchange.com>
2022-07-26 11:31:14 +0200

    create stream-related types
    Created simpleblob.NewReader and simpleblob.NewWriter functions, along with ReaderStorage and WriterStorage interface types.
    Those functions allow to provide an optimized way to write to and read from backend if those interfaces are satsfied.
    If an interface is not satisfied by backend, a fallback implementation is used, akin to `fs.GlobFS`.

---
+++ stream.go
@@ -0,0 +1,71 @@
+package simpleblob
+
+import (
+	"bytes"
+	"context"
+	"io"
+)
+
+// A ReaderStorage is an Interface providing an optimized way to create an io.ReadCloser.
+type ReaderStorage interface {
+	Interface
+	// NewReader returns an io.ReadCloser, allowing stream reading
+	// of named value from the underlying backend.
+	NewReader(context.Context, string) (io.ReadCloser, error)
+}
+
+// A ReaderStorage is an Interface providing an optimized way to create an io.WriteCloser.
+type WriterStorage interface {
+	Interface
+	// NewWriter returns an io.WriteCloser, allowing stream writing
+	// to named key in the underlying backend.
+	NewWriter(context.Context, string) (io.WriteCloser, error)
+}
+
+// A reader wraps a backend to satisfy io.ReadCloser.
+type reader struct {
+	st Interface
+	*bytes.Reader
+}
+
+// Close signifies operations on reader are over.
+func (*reader) Close() error {
+	return nil
+}
+
+// NewReader returns an optimized io.ReadCloser for backend if available,
+// else a basic buffered implementation.
+func NewReader(ctx context.Context, st Interface, name string) (io.ReadCloser, error) {
+	if sst, ok := st.(ReaderStorage); ok {
+		return sst.NewReader(ctx, name)
+	}
+	b, err := st.Load(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+	return &reader{st, bytes.NewReader(b)}, nil
+}
+
+// A writer wraps a backend to satisfy io.WriteCloser.
+// The bytes written to it are buffered, then sent to backend when closed.
+type writer struct {
+	st   Interface
+	ctx  context.Context
+	name string
+	*bytes.Buffer
+}
+
+// Close signifies operations on writer are over.
+// The file is sent to backend when called.
+func (w *writer) Close() error {
+	return w.st.Store(w.ctx, w.name, w.Bytes())
+}
+
+// NewWriter returns an optimized io.WriteCloser for backend if available,
+// else a basic buffered implementation.
+func NewWriter(ctx context.Context, st Interface, name string) (io.WriteCloser, error) {
+	if sst, ok := st.(WriterStorage); ok {
+		return sst.NewWriter(ctx, name)
+	}
+	return &writer{st, ctx, name, bytes.NewBuffer(nil)}, nil
+}
