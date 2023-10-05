842b09d22bbefd4320abea96010edf728ea2e5d2
Rodrigue Geis <rodrigue.geis@open-xchange.com>
2023-10-05 17:01:30 +0200

    s3: make a comment about global prefix use with marker clearer

--- backends/s3/marker.go
+++ backends/s3/marker.go
@@ -18,7 +18,7 @@ func (b *Backend) setMarker(ctx context.Context, name, etag string, isDel bool) 
 	}
 	nanos := time.Now().UnixNano()
 	s := fmt.Sprintf("%s:%s:%d:%v", name, etag, nanos, isDel)
-	// Here, we're not using Store because we don't want to add the global prefix.
+	// Here, we're not using Store because markerName already has the global prefix.
 	_, err := b.doStore(ctx, b.markerName, []byte(s))
 	if err != nil {
 		return err
