8910e74a623843ee5dba3815a612089c785a9948
Neil Cook <neil.cook@noware.co.uk>
2025-10-28 17:09:24 +0000

    metrics: Additional metrics for S3 backend
    - New histogram metric measuring the duration of S3 calls by method
    - New counter recording errors by method and error type (this is intended to
      replace the existing metric that just records errors by method)

--- backends/s3/metrics.go
+++ backends/s3/metrics.go
@@ -26,10 +26,27 @@ var (
 		},
 		[]string{"method"},
 	)
+	metricCallErrorsType = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "storage_s3_call_error_by_type_total",
+			Help: "S3 API call errors by method and error type",
+		},
+		[]string{"method", "error"},
+	)
+	metricCallHistogram = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "storage_s3_call_duration_seconds",
+			Help:    "S3 API call duration by method",
+			Buckets: []float64{0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 30, 60},
+		},
+		[]string{"method"},
+	)
 )
 
 func init() {
 	prometheus.MustRegister(metricLastCallTimestamp)
 	prometheus.MustRegister(metricCalls)
 	prometheus.MustRegister(metricCallErrors)
+	prometheus.MustRegister(metricCallErrorsType)
+	prometheus.MustRegister(metricCallHistogram)
 }
