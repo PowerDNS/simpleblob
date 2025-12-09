c9a837068747721de9a4bc15f6388bc09a4d9a06
Rodrigue Geis <rodrigue.geis@open-xchange.com>
2025-12-09 17:14:12 +0100

    implement benchmarks for sorting

--- blobs_test.go
+++ blobs_test.go
@@ -1,6 +1,10 @@
 package simpleblob
 
 import (
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,3 +27,41 @@ func TestBlobListStats(t *testing.T) {
 	assert.Equal(t, blobs.Len(), 2)
 	assert.Equal(t, blobs.Size(), int64(300))
 }
+
+func BenchmarkBlobListSort(b *testing.B) {
+	var blobList BlobList
+	for i := range 100 {
+		blobList = append(blobList, Blob{Name: strconv.Itoa(i), Size: int64(i)})
+	}
+	checkSorted := func() {
+		if !slices.IsSortedFunc(blobList, func(a, b Blob) int {
+			return strings.Compare(a.Name, b.Name)
+		}) {
+			b.Fatal("did not sort")
+		}
+	}
+
+	b.Run("sort.Sort", func(b *testing.B) {
+		s := sortable(blobList)
+		for b.Loop() {
+			slices.Reverse(blobList)
+			sort.Sort(s)
+			checkSorted()
+		}
+	})
+	b.Run("slices.SortFunc", func(b *testing.B) {
+		for b.Loop() {
+			slices.Reverse(blobList)
+			blobList.Sort()
+			checkSorted()
+		}
+	})
+}
+
+type sortable BlobList
+
+func (s sortable) Len() int               { return len(s) }
+func (s sortable) Less(i int, j int) bool { return s[i].Name < s[j].Name }
+func (s sortable) Swap(i int, j int)      { s[i], s[j] = s[j], s[i] }
+
+var _ sort.Interface = (sortable)(nil)
