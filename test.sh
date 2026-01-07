5d120ddb0e54f3045016011441f18350b739861a
Rodrigue Geis <rodrigue.geis@open-xchange.com>
2026-01-07 15:56:24 +0100

    tests: basic support for podman with testcontainers

--- test.sh
+++ test.sh
@@ -1,18 +1,56 @@
 #!/usr/bin/env sh
+# Run all tests for simpleblob.
+# The arguments passed to this script will be passed to the `go test` command.
+#
+# Note that the tests rely on testcontainers.
+# In case no Docker daemon is present, we fallback to Podman.
+# Using Podman can be forced by setting the SB_TESTS_FORCE_PODMAN to a non-empty value.
+set -e
 
-set -ex
+# Default to Docker but use Podman if required.
+if [ "$SB_TESTS_FORCE_PODMAN" ] || { [ -z "$DOCKER_HOST" ] && [ ! -e "/var/run/docker.sock" ]; }; then
+    # See https://podman-desktop.io/tutorial/testcontainers-with-podman#setup-testcontainers-with-podman
+    if ! command -v podman >/dev/null; then
+        echo "Neither Podman CLI or a Docker daemon were found."
+        exit 1
+    fi
+    case "$(uname -s)" in
+        Linux)
+            DOCKER_HOST="unix://${XDG_RUNTIME_DIR:-/run/user/$(id --user)}/podman/podman.sock"
+            ;;
+        Darwin)
+            DOCKER_HOST="unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')"
+            ;;
+        *)
+            echo "$(uname -s) is not supported."
+            exit 1
+            ;;
+    esac
+    if [ ! -e "${DOCKER_HOST#unix://}" ]; then
+        echo "Podman does not seem to be ready to accept connections."
+        echo "Please ensure you have started a Podman machine or system service."
+        # On macOS, this is most likely to be `podman machine start`.
+        # On Linux, you may have a service available, or you can run `podman system service --time=0`.
+        exit 1
+    fi
+    # TESTCONTAINERS_RYUK_DISABLED improves stability because the Ryuk container,
+    # used for cleanup, does not always work with Podman.
+    export TESTCONTAINERS_RYUK_DISABLED=true
+    export DOCKER_HOST
+fi
 
+echo 'Running tests'
 go test -count=1 "$@" ./...
 
-# Run linters
-# Configure linters in .golangci.yml
+echo
+echo 'Running linters'
 expected_version=2.4.0
-expected_version_full=v"$expected_version"
 golangci_lint_bin=./bin/golangci-lint
 if ! "$golangci_lint_bin" version | grep -wq "version $expected_version"; then
     # This is the recommended installation process.
     # The installer downloads to ./bin/ by default.
     # see https://golangci-lint.run/usage/install/
-    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s "$expected_version_full"
+    echo "Downloading golangci-lint v$expected_version to ./bin/"
+    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v"$expected_version"
 fi
-"$golangci_lint_bin" run
+exec "$golangci_lint_bin" run
