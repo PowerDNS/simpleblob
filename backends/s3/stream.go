2b0f1883328ff520c1dc0603251842afd0a7ac64
Rodrigue Geis <rodrigue.geis@open-xchange.com>
2026-04-01 16:56:55 +0200

    slightly better comments

--- backends/s3/stream.go
+++ backends/s3/stream.go
@@ -46,7 +46,7 @@ type writerWrapper struct {
 }
 
 // Write sends p to store as the S3 object associated with w.
-// An error is returned if Write failed previously, an error occured on the S3 side, or w is already closed.
+// An error is returned if Write failed previously, an error occured in S3, or w is already closed.
 func (w *writerWrapper) Write(p []byte) (n int, err error) {
 	// Not checking the status of ctx explicitly because it will be propagated
 	// from the reader goroutine.
@@ -55,13 +55,13 @@ func (w *writerWrapper) Write(p []byte) (n int, err error) {
 }
 
 // Close ensures that the written data is saved.
-// An error is returned if Write failed previously, an error occured on the S3 side, or w is already closed.
+// An error is returned if Write failed previously, an error occured in S3, or w is already closed.
 func (w *writerWrapper) Close() (err error) {
 	err = w.prevErr
 	_ = w.pw.CloseWithError(err)
 	if w.prevErr == nil {
 		w.prevErr = simpleblob.ErrClosed
 	}
-	<-w.ctx.Done()
+	<-w.ctx.Done() // cancelled after writing the marker
 	return err
 }
